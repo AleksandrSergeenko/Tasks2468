45702defeedbbd0bfa9fabf0113c835e15eb8c71
AleksandrSergeenko <122687809+AleksandrSergeenko@users.noreply.github.com>
2023-02-12 16:41:29 +0300

    Add решение Task8

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,19 +1,11 @@
-﻿/*Task6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-4 -> да
--3 -> нет
-7 -> нет
+﻿/*
+Task8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
 */
-
 int N;
-Console.Write("Число чётное? Ведите число: ");
-bool flag = int.TryParse(Console.ReadLine()!, out N);
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
 
-if (flag)
-{
-    if (N % 2 == 0)
-        Console.Write($"{N} -> да");  
-    else
-        Console.Write($"{N} -> нет");
-}
-else
-    Console.Write("Неверный ввод");
+for (int i = 2; i <= N; i += 2)
+    Console.Write($"{i} ");
